8d4037cac617bf6b35a07ee7eb8e9c964097fa98
ArtiomRamazanovgit config --global user.email tiomas15@gmail.comgit config --global --listgit --versiongit config --global user.name ArtiomRamazanov <tiomas15@gmail.com>
2025-04-10 12:55:37 +0300

    dafqq

--- js/components/header.js
+++ js/components/header.js
@@ -1,16 +1,17 @@
 export function header() {
+    let base = 'http://localhost:5408/';
     if (location.hostname !== 'localhost') {
-        const base = 'https://arramazanow.github.io/DOM/';
-        document.head.insertAdjacentHTML(`afterbegin', '<base href="${base}">`);
+        base = 'https://arramazanow.github.io/DOM/';
     }
+    document.head.insertAdjacentHTML('afterbegin', `<base href="${base}">`);
 
     const menu = [
-        { text: 'Home', href: './' },
-        { text: 'Text', href: './text' },
-        { text: 'Food', href: './food' },
-        { text: 'Darzas', href: './darzas' },
-        { text: 'Header', href: './header' },
-        { text: 'Click', href: './click' },
+        { text: 'Home', href: '/' },
+        { text: 'Text', href: '/text/' },
+        { text: 'Food', href: '/food/' },
+        { text: 'Darzas', href: '/darzas/' },
+        { text: 'Header', href: '/header/' },
+        { text: 'Click', href: '/click/' },
     ];
 
     const lp = location.pathname;
@@ -28,7 +29,7 @@ export function header() {
 
     const HTML = `
         <header class="main-header">
-            <img class="logo" src="./food/pizza.png" alt="Logo">
+            <img class="logo" src="./img/logo.png" alt="Logo">
             <nav class="main-nav">${linksHTML}</nav>
         </header>`;
 
