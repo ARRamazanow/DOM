ac42aa836c9bf0412815abc99fd7f584f023f26d
ArtiomRamazanovgit config --global user.email tiomas15@gmail.comgit config --global --listgit --versiongit config --global user.name ArtiomRamazanov <tiomas15@gmail.com>
2025-04-10 12:36:29 +0300

    dafq

--- js/components/header.js
+++ js/components/header.js
@@ -1,11 +1,16 @@
 export function header() {
+    if (location.hostname !== 'localhost') {
+        const base = 'https://arramazanow.github.io/DOM/';
+        document.head.insertAdjacentHTML(`afterbegin', '<base href="${base}">`);
+    }
+
     const menu = [
-        { text: 'Home', href: '/' },
-        { text: 'Text', href: '/text' },
-        { text: 'Food', href: '/food' },
-        { text: 'Darzas', href: '/darzas' },
-        { text: 'Header', href: '/header' },
-        { text: 'Click', href: '/click' },
+        { text: 'Home', href: './' },
+        { text: 'Text', href: './text' },
+        { text: 'Food', href: './food' },
+        { text: 'Darzas', href: './darzas' },
+        { text: 'Header', href: './header' },
+        { text: 'Click', href: './click' },
     ];
 
     const lp = location.pathname;
@@ -23,7 +28,7 @@ export function header() {
 
     const HTML = `
         <header class="main-header">
-            <img class="logo" src="../food/pizza.png" alt="Logo">
+            <img class="logo" src="./food/pizza.png" alt="Logo">
             <nav class="main-nav">${linksHTML}</nav>
         </header>`;
 
